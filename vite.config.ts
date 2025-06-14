f4b9d377211fcfdb24cd6c7f38059cee892071b2
Felipe Cock Martínez <26395118+felipecock@users.noreply.github.com>
2025-06-14 14:27:43 -0500

    fix: establecer la base en la configuración de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/answer-for-me/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
